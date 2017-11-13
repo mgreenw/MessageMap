9b2061e6c9063d44a135bae4fd9f332b341720bc
Max Greenwald <maxgreenwald@me.com>
2017-11-12 19:21:46 -0500

    Initial Commit

---
+++ MessageMap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MessageMap
+//
+//  Created by Max Greenwald on 11/12/17.
+//  Copyright © 2017 Max Greenwald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MessageMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MessageMap
+//
+//  Created by Max Greenwald on 11/12/17.
+//  Copyright © 2017 Max Greenwald. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
